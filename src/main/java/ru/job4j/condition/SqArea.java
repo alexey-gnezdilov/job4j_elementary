518c410fce5efdc32fb7a82c7804d40363d34777
alexey-gnezdilov <gnezdilov_alexey_1993@mail.ru>
2021-12-17 08:17:47 +0800

    Площадь треугольника. [#175474] Стороны прямоугольника. [#137735 #240675] Расстояние между точками в системе координат [#188]

--- src/main/java/ru/job4j/condition/SqArea.java
+++ src/main/java/ru/job4j/condition/SqArea.java
@@ -2,14 +2,13 @@ package ru.job4j.condition;
 
 public class SqArea {
     public static double square(double p, double k) {
-        double rsl = ((Math.pow(p, 2)) * k) / (4 * (Math.pow((k + 1), 2)));
-        return rsl;
+        double h = p / (2 * (k + 1));
+        double l = h * k;
+        return l * h;
     }
 
     public static void main(String[] args) {
-        double result1 = SqArea.square(4, 1);
-        System.out.println(" p = 4, k = 1, s = 1, real = " + result1);
-        result1 = SqArea.square(6, 2);
-        System.out.println(" p = 6, k = 2, s = 2, real = " + result1);
+        System.out.println(" p = 4, k = 1, s = 1, real = " + SqArea.square(4, 1));
+        System.out.println(" p = 6, k = 2, s = 2, real = " + SqArea.square(6, 2));
     }
 }
