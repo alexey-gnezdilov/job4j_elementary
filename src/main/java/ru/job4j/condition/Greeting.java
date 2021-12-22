0d48979e74b08f84b0f1d239b4f1413d196b6a1b
alexey-gnezdilov <gnezdilov_alexey_1993@mail.ru>
2021-12-22 18:27:37 +0800

    1

--- src/main/java/ru/job4j/condition/Greeting.java
+++ src/main/java/ru/job4j/condition/Greeting.java
@@ -2,11 +2,10 @@ package ru.job4j.condition;
 
 public class Greeting {
     public static void main(String[] args) {
-        String idea = "I like Java!";
-        System.out.println(idea);
-        idea += " " + "But I am a newbie.";
-        int year = 2021;
-        idea += " " + year;
-        System.out.println(idea);
+
+        short s = 1500;
+        char c = (char) s;
+        double d = c;
+        System.out.println(d);
     }
 }
